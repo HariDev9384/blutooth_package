209cf5c3d58975080ba462e87848948ca60f2d9b
Hari <hariharan@zed.digital>
2023-07-28 17:46:14 +0530

    permission_crash

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 
 import 'utils/common.dart';
-
+  Future<void> requestLocationPermission() async {
+  var locationStatus = await Permission.location.request();
+  var nearbyStatus = await  Permission.nearbyWifiDevices.request();
+  await Permission.bluetoothScan.request();
+  await Permission.bluetooth.request();
+  await Permission.bluetoothAdvertise.request();
+  await Permission.bluetoothConnect.request();
+  if (locationStatus.isDenied||nearbyStatus.isDenied) {
+    // Handle denied permission
+  } else if (locationStatus.isPermanentlyDenied||nearbyStatus.isPermanentlyDenied) {
+    // Handle permanently denied permission
+  } else if (locationStatus.isGranted||nearbyStatus.isGranted) {
+    // Permission is granted, proceed with Wi-Fi Direct interactions
+  }
+}
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Permission.location.request().then(
-    (v){
-        runApp(const MyApp());
-    }
-  );
+  requestLocationPermission().then((value) => runApp(const MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -18,14 +28,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'blue',
-      
       home: Home()
     );
   }
